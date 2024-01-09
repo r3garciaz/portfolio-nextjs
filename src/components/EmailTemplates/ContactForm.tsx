c04504b240c56e22b9e0107e7e5a746c7c546b35
reinaldo <garcia.reinaldo@gmail.com>
2024-01-09 18:13:04 -0300

    wrapped in Reac.memo

--- src/components/EmailTemplates/ContactForm.tsx
+++ src/components/EmailTemplates/ContactForm.tsx
@@ -1,27 +1,24 @@
-import {FC} from 'react';
+import React, {FC, memo} from 'react';
 
 import {EmailTemplateProps} from '../../data/dataDef';
 
-export const EmailTemplate: FC<Readonly<EmailTemplateProps>> = ({
-  name,
-  email,
-  message
-}) => (
-  <div>
-    <p>Hello r3garciaz,</p>
-    <p>
-      {name} has submitted the contact form on your website. Their
-      email is {email}!
-    </p>
-    <p>
-      Their message is:
-      <br />
-      {message}
-    </p>
-    <p>
-      Regards,
-      <br />
-      Coffee
-    </p>
-  </div>
-);
+export const EmailTemplate: FC<Readonly<EmailTemplateProps>> = memo(props=> {
+  const {name, email, message} = props;
+
+  return (
+    <div>
+      <p>Hello r3garciaz,</p>
+      <p> {name} has submitted the contact form on your website. Their email is {email}! </p>
+      <p>
+        Their message is:
+        <br />
+        {message}
+      </p>
+      <p>
+        Regards,
+        <br />
+        Coffee
+      </p>
+    </div>
+  );
+});
